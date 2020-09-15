c98ade986ab6e5410d2e1c8fabf5faff0586d9e4
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-09-15 16:56:11 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,7 +15,7 @@ end
 
 def apply_discount
 applied_discount = (100 - @discount) * 0.01
-@total = applied_discount * @total 
+@total = applied_discount * @total
 return "After the discount, the total comes to #{@total}"
 end
 
