02de0c56d0e9146632470bbad0f92ce065d75f48
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-09-15 16:01:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+require 'pry'
+class CashRegister
 
+  attr_accessor :new, :discount, :total
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    items = []
+  end
+
+def add_item(title,price,quantity = 1)
+@total += price * quantity
+end
+
+def apply_discount
+binding.pry
+@total -= (@discount/10.to_f)
+end  #expected to get 800 and the total is 10000 without the discount
+
+end
