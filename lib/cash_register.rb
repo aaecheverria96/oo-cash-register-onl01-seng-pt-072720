ce78afacc406e3e24aa764f834ffd131d076f9a6
Andrew Echeverria <andrew.a.echeverria@gmail.com>
2020-09-15 18:55:45 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -12,7 +12,7 @@ class CashRegister
 
 def add_item(title,price,quantity = 1)
 @total += price * quantity
-@total = @last_total 
+@total = @last_total
 quantity.times do items << title
 end
 end
@@ -32,7 +32,7 @@ def items
 end
 
 def void_last_transaction
-@total = @last_total 
+@total = @last_total
 end
 
 end
